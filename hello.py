205ef0c480c318ec8ca0202a5f4c8f11f0d3dc01
สากล บู่ทอง <sakon@transcodeThailand.onmicrosoft.com>
2021-05-12 12:32:47 +0700

    other version

---
+++ hello.py
@@ -0,0 +1,10 @@
+msg = "Hello World"
+print(msg)
+
+"""
+During Debugging
+Type
+msg 
+msg.split()
+msg.capitalize()
+"""
---
+++ shortcuts.py
@@ -0,0 +1,14 @@
+#crtl+c, x ที่ว่าง ๆ จะ เลือกทั้งบรรทัด
+#alt+arrow up or down , จะเลื่อนข้อความทั้งบรรทัดขึ้นลงได้
+#ctrl+] or [ indent, จะ indent แบบ cursor อยู่ที่ไหนก็ได้
+#multi cursor mode
+#ยังไม่ work -- ให้กด ctrl ค้างไว้แล้วกดเลือกหลาย ๆ ที่จะสามารถเติมคำหรือลบครั้งเดียวได้ทั้งหมด
+def q(a, b, c):
+    return a*b*c
+#ctrl+shift+enter, ctrl+enter เพื่อแทรกบรรทัด
+#ctrl+/ เพื่อใส่ comment
+# comment
+#ctrl+shift+alt+downarrow เพื่อ multi cursor คือ เลือกหลายบรรทัด
+here_name1 = 'Perter'
+here_name2 = 'Ann'
+here_name3 = 'Bruce'
